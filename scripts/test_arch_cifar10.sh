c734c6f114b4c969ba5f13117f56a8b45ecc1e52
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2023-06-09 15:10:34 -0400

    Add files via upload

---
+++ scripts/test_arch_cifar10.sh
@@ -0,0 +1,2 @@
+
+python MGPU_test_arch.py --gpu_ids 0 --num_workers 8 --dataset cifar10 --bottom_width 4 --img_size 32 --arch arch_cifar10 --draw_arch False --checkpoint arch_train_cifar10_MMD_TrainfromMMD_LargeDisc --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 256 --df_dim 512 --num_eval_imgs 50000 --eval_batch_size 100 --exp_name arch_test_cifar10
