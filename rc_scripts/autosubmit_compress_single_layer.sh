8e403b1450066147df1d6afe956464186b1324e3
MM:RC <mm3424@rit.edu>
2023-07-25 21:37:31 -0400

    minor fix in scripts

--- rc_scripts/autosubmit_compress_single_layer.sh
+++ rc_scripts/autosubmit_compress_single_layer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-layers=('cell1.c0.ops.0.op.1' 'cell1.c1.ops.0.op.1' 'cell1.c2.ops.0.op.1' 'cell1.c3.ops.0.op.1' 'cell2.c0.ops.0.op.1' 'cell2.c2.ops.0.op.1' 'cell2.c3.ops.0.op.1' 'cell2.c4.ops.0.op.1'  'cell3.c0.ops.0.op.1' 'cell3.c1.ops.0.op.1' 'cell3.c2.ops.0.op.1' 'cell3.c3.ops.0.op.1' 'cell3.c4.ops.0.op.1')
+layers=('cell1.c0.ops.0.op.1' 'cell1.c1.ops.0.op.1') # 'cell1.c2.ops.0.op.1' 'cell1.c3.ops.0.op.1' 'cell2.c0.ops.0.op.1' 'cell2.c2.ops.0.op.1' 'cell2.c3.ops.0.op.1' 'cell2.c4.ops.0.op.1'  'cell3.c0.ops.0.op.1' 'cell3.c1.ops.0.op.1' 'cell3.c2.ops.0.op.1' 'cell3.c3.ops.0.op.1' 'cell3.c4.ops.0.op.1')
 
 
 epochs=400
@@ -17,6 +17,6 @@ for layer in ${layers[@]}; do
     EXPNAME="compL${layer_id}"
     echo $EXPNAME
     sbatch --output=Results/%x_%j.out --error=Results/%x_%j.err --time=1-0:0:0 --job-name=comL${layer_id} --export=CHECKPOINT=$checkpoint,BU=$bu,LAYER=$layer,R=$R,EXPNAME=$EXPNAME,MAXEPOCH=$maxepoch rc_scripts/compress_single_layer_nofreeze_template.sh
-    sbatch --output=Results/%x_%j.out --error=Results/%x_%j.err --time=1-0:0:0 --job-name=comL${layer_id}frz --export=CHECKPOINT=$checkpoint,BU=$bu,LAYER=$layer,R=$R,EXPNAME=$EXPNAME,MAXEPOCH=$maxepoch,FREEZE_LAYERS=$layer rc_scripts/compress_single_layer_nofreeze_template.sh
+    sbatch --output=Results/%x_%j.out --error=Results/%x_%j.err --time=1-0:0:0 --job-name=comL${layer_id}frz --export=CHECKPOINT=$checkpoint,BU=$bu,LAYER=$layer,R=$R,EXPNAME=$EXPNAME,MAXEPOCH=$maxepoch,FREEZE_LAYERS=$layer rc_scripts/compress_single_layer_freeze_template.sh
 done
 
