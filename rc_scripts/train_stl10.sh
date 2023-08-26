c5aba450227e00c927f72e614d7ef03a709c3b60
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2023-08-26 16:42:42 -0400

    Add files via upload

--- rc_scripts/train_stl10.sh
+++ rc_scripts/train_stl10.sh
@@ -14,7 +14,7 @@
 
 # 5 days is the run time MAX, anything over will be KILLED unless you talk with RC
 # Time limit days-hrs:min:sec
-#SBATCH --time=1-0:0:0
+#SBATCH --time=5-0:0:0
 
 # Put the job in the appropriate partition matchine the account and request FOUR cores
 #SBATCH --account=vividgan   #this is the name created for your project when you filled out the questionnaire
@@ -26,11 +26,10 @@
 #SBATCH --mem=32g
 #SBATCH --gres=gpu:a100:1
 
-
 spack env activate tensors-23062101
 
-# Train Large Network
-python MGPU_train_arch.py --gpu_ids 0 --num_workers 8 --gen_bs 128 --dis_bs 128 --dataset cifar10 --bottom_width 4 --img_size 32 --max_epoch_G 500 --n_critic 1 --arch arch_cifar10 --draw_arch False --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 256 --df_dim 512 --g_lr 0.0002 --d_lr 0.0002 --beta1 0.0 --beta2 0.9 --init_type xavier_uniform --val_freq 5 --num_eval_imgs 50000 --exp_name arch_train_cifar10_large
+# Train Large Generator
+python MGPU_train_arch.py --gpu_ids 0 --num_workers 18 --gen_bs 128 --dis_bs 128 --dataset stl10 --bottom_width 6 --img_size 48 --max_epoch_G 500 --n_critic 1 --arch arch_cifar10 --draw_arch False --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 256 --df_dim 512 --g_lr 0.0002 --d_lr 0.0002 --beta1 0.0 --beta2 0.9 --init_type xavier_uniform --val_freq 5 --num_eval_imgs 50000 --exp_name arch_train_stl10_large
 
-# Train Small Network
-python MGPU_train_arch.py --gpu_ids 0 --num_workers 8 --gen_bs 128 --dis_bs 128 --dataset cifar10 --bottom_width 4 --img_size 32 --max_epoch_G 500 --n_critic 1 --arch arch_cifar10 --draw_arch False --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --df_dim 512 --g_lr 0.0002 --d_lr 0.0002 --beta1 0.0 --beta2 0.9 --init_type xavier_uniform --val_freq 5 --num_eval_imgs 50000 --exp_name arch_train_cifar10_small
+# Train Small Generator
+python MGPU_train_arch.py --gpu_ids 0 --num_workers 18 --gen_bs 128 --dis_bs 128 --dataset stl10 --bottom_width 6 --img_size 48 --max_epoch_G 500 --n_critic 1 --arch arch_cifar10 --draw_arch False --genotypes_exp arch_cifar10 --latent_dim 120 --gf_dim 128 --df_dim 512 --g_lr 0.0002 --d_lr 0.0002 --beta1 0.0 --beta2 0.9 --init_type xavier_uniform --val_freq 5 --num_eval_imgs 50000 --exp_name arch_train_stl10_small
