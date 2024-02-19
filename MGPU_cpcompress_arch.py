849af2e8fba080e4686c807d343f9d9994d7d563
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2024-02-19 13:23:29 -0500

    Update MGPU_cpcompress_arch.py

--- MGPU_cpcompress_arch.py
+++ MGPU_cpcompress_arch.py
@@ -31,6 +31,7 @@ def main():
     args = cfg_compress.parse_args()
     validate_args(args)
     torch.cuda.manual_seed(args.random_seed)
+    np.random.seed(args.random_seed)
 
     # set visible GPU ids
     if len(args.gpu_ids) > 0:
